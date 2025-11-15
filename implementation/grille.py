1b49f8327fd43b162795d9f64e6bc46ee706c209
MLoeva <loeva.travail@gmail.com>
2025-11-15 13:50:42 +0100

    correction fonction calcul_case_numero()

--- implementation/grille.py
+++ implementation/grille.py
@@ -10,6 +10,23 @@ import random as rd
 class Grille(object):
     
     def __init__(self, nb_lignes, nb_colonnes, nb_mines):
+        """
+        Initialise une nouvelle instance du jeu de démineur.
+        
+        Paramètres
+        ----------
+        nb_lignes : int
+            Nombre de lignes de la grille.
+        nb_colonnes : int
+            Nombre de colonnes de la grille.
+        nb_mines : int
+            Nombre total de mines à placer dans la grille.
+    
+        Description
+        -----------
+        - Création de la grille visible par le joueur, remplie de 'X' (cases non révélées).
+        - Initialisation de la liste des positions de mines (qui sera remplie plus tard).
+        - Création de la grille contenant les indices de présence des bombes (initialisée avec des zéros, remplie plus tard) """
         self.nb_lignes = nb_lignes
         self.nb_colonnes = nb_colonnes
         self.grille_visible = np.array(['X' for _ in range(self.nb_lignes*self.nb_colonnes)]).reshape((self.nb_lignes,self.nb_colonnes))
@@ -21,23 +38,38 @@ class Grille(object):
     def __str__(self):
         return str(self.grille_visible)
     
-    def calcul_position_mines(self):
-        """
-        Calcule la position des mines dans la grille de jeu
-        """
-        for i in range(self.nb_mines):
-            x = rd.randint(0, self.nb_lignes-1)
-            y = rd.randint(0, self.nb_colonnes-1)
+    # def calcul_position_mines(self):
+    #     """
+    #     Calcule la position des mines dans la grille de jeu
+    #     """
+    #     for i in range(self.nb_mines):
+    #         x = rd.randint(0, self.nb_lignes-1)
+    #         y = rd.randint(0, self.nb_colonnes-1)
             
-            if [x,y] not in self.position_mines :
-                self.position_mines.append([x,y])
-                self.grille_numeros[x,y]=9
+    #         if [x,y] not in self.position_mines :
+    #             self.position_mines.append([x,y])
+    #             self.grille_numeros[x,y]=9
           
-            else : 
-                i -=1 #ou mettre un while à la place du for
+    #         else : 
+    #             i -=1 #ou mettre un while à la place du for
+                
+    def calcul_position_mines(self):
+        mines_placees = 0
+
+        while mines_placees < self.nb_mines:
+            x = rd.randint(0, self.nb_lignes - 1)
+            y = rd.randint(0, self.nb_colonnes - 1)
+    
+            if [x, y] not in self.position_mines:
+                self.position_mines.append([x, y])
+                self.grille_numeros[x, y] = 9
+                mines_placees += 1
     
     def calcul_cases_numero(self):
-
+        """
+        Calcule le nombre de bombes au voisinage de chaque case de la grille
+        """
+        
         for mine in self.position_mines :
             x = mine[0]
             y = mine[1]
@@ -47,68 +79,26 @@ class Grille(object):
                     if dx==0 and dy==0:
                         continue
                     
-                    if 0<x+dx<self.nb_lignes and 0<y+dy<self.nb_lignes:
+                    if -1<x+dx<self.nb_lignes and -1<y+dy<self.nb_lignes:
                         self.grille_numeros[x+dx, y+dy]+=1
-                        
-            
-            # try :
-            #     if y!=self.nb_colonnes-1:
-            #         self.grille_numeros[x+1, y]+=1 #à droite de la mine
-            # except IndexError:
-            #     pass
-            
-            # try :
-            #     if y!=0:
-            #         self.grille_numeros[x-1, y]+=1 #à gauche de la mine #ca marche !
-            # except IndexError:
-            #     pass
-            
-            # try:
-            #     #pour la ligne au dessus de la mine
-            #     if x!=0:
-            #         self.grille_numeros[x-1, y-1]+=1 
-            #         self.grille_numeros[x, y-1]+=1
-            #         self.grille_numeros[x+1, y-1]+=1
-            # except IndexError:
-            #     pass
-            
-            # try :
-            #     #pour la ligne au dessous de la mine
-            #     if x!=self.nb_colonnes-1:
-            #         self.grille_numeros[x-1, y+1]+=1
-            #         self.grille_numeros[x, y+1]+=1
-            #         self.grille_numeros[x+1, y+1]+=1
-            # except IndexError : 
-            #     pass
-        
-        #pour avoir une grille propre
+   
+        #pour avoir une grille_numeros propre, avec seulement des numéros de 0 à 9, car si 2 bombes voisines, elle prennent la valeur 10.
         for mine in self.position_mines :
             self.grille_numeros[mine[0], mine[1]]=9
-            
-    # def creuser(self, ligne, colonne):
-    #     numero_creuse = self.grille_numeros[ligne-1, colonne-1]
-    #     if numero_creuse == 9:
-    #         return True #j'ai perdu
-        
-    #     if numero_creuse == 0:
-    #         self.grille_visible[ligne-1, colonne-1]= str(numero_creuse)
-    #         for i in range(-2, 1, 1):
-    #             for j in range(-2, 1, 1):
-    #                 try :
-    #                     numero=self.grille_numeros[ligne+i, colonne+j]
-    #                     if numero!=9:
-    #                         self.grille_visible[ligne+i, colonne+j]= str(numero)
-                        
-    #                 except IndexError : 
-    #                     pass                
-    #         return False
-        
-    #     else :    
-    #         self.grille_visible[ligne-1, colonne-1]= str(numero_creuse)
-    #         return False
-
+ 
 
     def creuser(self, ligne, colonne):
+        """
+        Révèle le numéro caché sous la case cliquée.
+        Autrement dit, lorsque le joueur clioque sur une case 'X' de grille_visible,
+        la valeur de la case est remplacée par la valeur contenue dans grille_numeros.
+        
+        ligne : integer
+        colonne : integer
+        
+        Return : True si le joueur découvre une mine, il a perdu
+                False sinon, le jeu continue
+        """
         numero_creuse = self.grille_numeros[ligne, colonne]
         if numero_creuse == 9:
             return True #j'ai perdu
@@ -123,6 +113,19 @@ class Grille(object):
             return False
     
     def creuse_autour(self, ligne, colonne):
+        """
+
+        Parameters
+        ----------
+        ligne : integer
+        colonne : integer
+
+
+        Returns
+        -------
+        None.
+
+        """
         a_creuser_autour = [[ligne, colonne]] 
         
         for case_creusee in a_creuser_autour:
